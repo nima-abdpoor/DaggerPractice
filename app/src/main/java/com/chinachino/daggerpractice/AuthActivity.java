8882e9fa06f8e103909c9207cb3389cb9fd20512
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-19 18:29:32 +0430

    inject a String to the BaseActivity

---
+++ app/src/main/java/com/chinachino/daggerpractice/AuthActivity.java
@@ -0,0 +1,25 @@
+package com.chinachino.daggerpractice;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import javax.inject.Inject;
+
+import dagger.android.support.DaggerAppCompatActivity;
+
+public class AuthActivity extends DaggerAppCompatActivity {
+
+    private static final String TAG = "AuthActivity";
+
+    @Inject
+    String asljflas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+        Log.d(TAG, "onCreate: " + asljflas);
+    }
+
+
+}
