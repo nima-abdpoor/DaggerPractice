5476cd2eeb0f2e63c80f68d00d55079b90d22f51
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-19 17:52:54 +0430

    AppComponent and BaseActivity

---
+++ app/src/main/java/com/chinachino/daggerpractice/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.chinachino.daggerpractice;
+
+import com.chinachino.daggerpractice.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseActivity extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().Application(this).build();
+    }
+}
---
+++ app/src/main/java/com/chinachino/daggerpractice/di/AppComponent.java
@@ -0,0 +1,27 @@
+package com.chinachino.daggerpractice.di;
+
+import android.app.Application;
+
+import com.chinachino.daggerpractice.BaseActivity;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseActivity> {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        Builder Application(Application application);
+
+        AppComponent build();
+    }
+
+}
