ee6248050deea4dfaa97e32761769a1453c72924
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-23 11:27:16 +0330

    show all data that profile needs in UI

--- app/src/main/java/com/chinachino/daggerpractice/UI/main/profile/ProfileFragment.java
+++ app/src/main/java/com/chinachino/daggerpractice/UI/main/profile/ProfileFragment.java
@@ -4,15 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.chinachino.daggerpractice.R;
 import com.chinachino.daggerpractice.ViewModels.ViewModelProviderFactory;
+import com.chinachino.daggerpractice.model.User;
 
 import javax.inject.Inject;
 
@@ -26,17 +27,52 @@ public class ProfileFragment extends DaggerFragment {
     @Inject
     ViewModelProviderFactory providerFactory;
 
+    //view
+    TextView email,username,website;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Toast.makeText(getActivity(), "profile fragment", Toast.LENGTH_SHORT).show();
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-       viewModel = ViewModelProviders.of(this,providerFactory).get(ProfileViewModel.class);
+        email = view.findViewById(R.id.email);
+        username = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
+        viewModel = ViewModelProviders.of(this, providerFactory).get(ProfileViewModel.class);
+        subscribeObservers();
+    }
 
+    public void subscribeObservers() {
+        viewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        viewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), userAuthResource -> {
+            if (userAuthResource != null) {
+                switch (userAuthResource.status) {
+                    case ERROR: {
+                        setError(userAuthResource.message);
+                        break;
+                    }
+                    case AUTHENTICATED: {
+                        setUserDetails(userAuthResource.data);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    private void setError(String message) {
+        email.setText(message);
+        username.setText("Error");
+        website.setText("Error");
+    }
+
+    private void setUserDetails(User data) {
+        email.setText(data.getEmail());
+        username.setText(data.getUsername());
+        website.setText(data.getWebsite());
     }
 }
--- app/src/main/java/com/chinachino/daggerpractice/UI/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/chinachino/daggerpractice/UI/main/profile/ProfileViewModel.java
@@ -2,15 +2,27 @@ package com.chinachino.daggerpractice.UI.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.chinachino.daggerpractice.SessionManager;
+import com.chinachino.daggerpractice.UI.Auth.AuthResource;
+import com.chinachino.daggerpractice.model.User;
+
+import java.net.UnknownServiceException;
 
 import javax.inject.Inject;
 
 public class ProfileViewModel extends ViewModel {
     private static final String TAG = "ProfileViewModel";
 
+    private final SessionManager sessionManager;
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         Log.d(TAG, "ProfileViewModel: view model is ready ... ");
+    }
+    public LiveData<AuthResource<User>> getAuthenticatedUser(){
+        return sessionManager.getMediatorLiveData();
     }
 }
