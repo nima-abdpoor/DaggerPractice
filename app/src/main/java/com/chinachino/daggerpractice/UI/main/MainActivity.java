ae79671fd2c934dc38271183475b6ab2fadb388b
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-24 17:21:07 +0330

    navigation drawer added to activity_main.xml

--- app/src/main/java/com/chinachino/daggerpractice/UI/main/MainActivity.java
+++ app/src/main/java/com/chinachino/daggerpractice/UI/main/MainActivity.java
@@ -8,21 +8,28 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.chinachino.daggerpractice.BaseActivity;
 import com.chinachino.daggerpractice.R;
-import com.chinachino.daggerpractice.UI.main.profile.ProfileFragment;
 import com.chinachino.daggerpractice.UI.post.PostFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show();
         testFragment();
     }
@@ -49,5 +56,20 @@ public class MainActivity extends BaseActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_post:{
+                break;
+            }
+            case R.id.nav_profile:{
+                break;
+            }
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
