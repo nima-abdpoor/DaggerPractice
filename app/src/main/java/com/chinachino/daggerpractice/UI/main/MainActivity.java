ae3a8e7c5ab858bfa2b0d656d113eb41c8df09d3
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-24 18:43:22 +0330

    navigation drawer and on item click listener.

--- app/src/main/java/com/chinachino/daggerpractice/UI/main/MainActivity.java
+++ app/src/main/java/com/chinachino/daggerpractice/UI/main/MainActivity.java
@@ -10,10 +10,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.chinachino.daggerpractice.BaseActivity;
 import com.chinachino.daggerpractice.R;
-import com.chinachino.daggerpractice.UI.post.PostFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -31,26 +33,27 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show();
-        testFragment();
+        init();
     }
 
-    private void testFragment() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_container,new PostFragment())
-                .commit();
+    public void init() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
+        inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.logout :{
+        switch (item.getItemId()) {
+            case R.id.logout: {
                 sessionManager.logOut();
                 return true;
             }
@@ -60,11 +63,12 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()){
-            case R.id.nav_post:{
+        switch (menuItem.getItemId()) {
+            case R.id.nav_post: {
                 break;
             }
-            case R.id.nav_profile:{
+            case R.id.nav_profile: {
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.profile_screen);
                 break;
             }
         }
