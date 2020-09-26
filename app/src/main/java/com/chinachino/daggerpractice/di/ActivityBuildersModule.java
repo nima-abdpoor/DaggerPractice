19a9151ceb6adda9d9a5a552fb51068d76ed50db
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-26 17:06:05 +0330

    defining AuthScope and MainScope for subComponents

--- app/src/main/java/com/chinachino/daggerpractice/di/ActivityBuildersModule.java
+++ app/src/main/java/com/chinachino/daggerpractice/di/ActivityBuildersModule.java
@@ -3,9 +3,11 @@ package com.chinachino.daggerpractice.di;
 import com.chinachino.daggerpractice.UI.Auth.AuthActivity;
 import com.chinachino.daggerpractice.UI.main.MainActivity;
 import com.chinachino.daggerpractice.di.auth.AuthModule;
+import com.chinachino.daggerpractice.di.auth.AuthScope;
 import com.chinachino.daggerpractice.di.auth.AuthViewModelsModule;
 import com.chinachino.daggerpractice.di.main.MainFragmentBuildersModule;
 import com.chinachino.daggerpractice.di.main.MainModule;
+import com.chinachino.daggerpractice.di.main.MainScope;
 import com.chinachino.daggerpractice.di.main.MainViewModelsModule;
 
 import dagger.Module;
@@ -14,12 +16,14 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector(
             modules = {AuthViewModelsModule.class
                     , AuthModule.class}
     )
     abstract AuthActivity ContributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {MainFragmentBuildersModule.class
                     , MainViewModelsModule.class
--- app/src/main/java/com/chinachino/daggerpractice/di/auth/AuthModule.java
+++ app/src/main/java/com/chinachino/daggerpractice/di/auth/AuthModule.java
@@ -8,6 +8,8 @@ import retrofit2.Retrofit;
 
 @Module
 public class AuthModule {
+
+    @AuthScope
     @Provides
     static AuthAPI provideAuthAPI(Retrofit retrofit){
         return retrofit.create(AuthAPI.class);
